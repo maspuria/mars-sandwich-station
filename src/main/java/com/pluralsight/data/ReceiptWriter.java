e8a864a033689e657dd3bbff9f4669b74ff3fdd3
mariannaaspuria <aspuria.mp@gmail.com>
2025-11-07 15:41:44 -0600

    pseudocode outline commit

--- src/main/java/com/pluralsight/data/ReceiptWriter.java
+++ src/main/java/com/pluralsight/data/ReceiptWriter.java
@@ -1,4 +1,24 @@
 package com.pluralsight.data;
 
+import com.pluralsight.model.Order;
+
 public class ReceiptWriter {
+    public static void saveReceipt(Order order) {
+        // generate filename using current date/time (yyyyMMdd-HHmmss.txt)
+        // create a FileWriter and wrap in BufferedWriter (src/main/resources/receipts)
+
+        // loop through all order items
+        //   - write the items to the to reciept
+
+        //write total cost
+
+        // close BufferedWriter
+        // handle IOException with error message
+    }
+
+    private static String generateTimestamp() {
+        // Create timestamp string useing a formatter
+        // return that string
+        return null; //temporary
+    }
 }
--- src/main/java/com/pluralsight/ui/UserInterface.java
+++ src/main/java/com/pluralsight/ui/UserInterface.java
@@ -1,4 +1,48 @@
 package com.pluralsight.ui;
 
+import java.util.Scanner;
+
 public class UserInterface {
+// declare a Scanner object for reading input from the console
+
+    // constructor:
+
+    // showHomeScreen:
+    //   - print app title and welcome message
+    //   - print:
+    //       1) New Order
+    //       0) Exit
+    //   - prompt user for choice
+    //   - return user input
+
+    // showOrderMenu:
+    //   - print:
+    //       1) Add Sandwich
+    //       2) Add Drink
+    //       3) Add Chips
+    //       4) Checkout
+    //       0) Cancel Order
+    //   - prompt user for choice
+    //   - return user input
+
+    // promptForBreadType:
+    //   - print list of bread types
+    //   - ask user to type one in (e.g., "white")
+    //   - return as user input
+
+    // promptForSize:
+    //   - ask user: "What size? (4, 8, or 12 inches)"
+    //   - return user input
+
+    // promptForToasted:
+    //   - ask: "Would you like it toasted? (yes or no)"
+    //   - return user input
+
+    // promptForMeats:
+    //   - possible loop (one or multiple meats?):
+    //       - ask user to type of meat (or 'done' to finish)
+    //       - ask if they want Extra meat? (yes or no)"
+    //   - return list of meats and extras
+
+    // and so on and so on........
 }
