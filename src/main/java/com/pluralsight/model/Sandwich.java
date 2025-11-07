c540ff2cf47cd1924b7293c0398e64e3ec3fd05d
mariannaaspuria <aspuria.mp@gmail.com>
2025-11-07 13:49:08 -0600

    sandwich class field members commit

---
+++ src/main/java/com/pluralsight/model/Sandwich.java
@@ -0,0 +1,18 @@
+package com.pluralsight.model;
+
+import java.util.List;
+
+public class Sandwich {
+    // Field Members
+    private String breadType;
+    private int size;
+    private boolean isToasted;
+    private List<String> meats;
+    private List<String> cheeses;
+    private List<String> toppings;
+    private List<String> sauces;
+    private double basePrice;
+    private double totalPrice;
+
+
+}
