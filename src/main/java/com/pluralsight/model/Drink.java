ac5cc33c40f49c7a87e9f917196c20f58325bad0
mariannaaspuria <aspuria.mp@gmail.com>
2025-11-10 14:12:40 -0600

    update drinks class commit

--- src/main/java/com/pluralsight/model/Drink.java
+++ src/main/java/com/pluralsight/model/Drink.java
@@ -1,4 +1,34 @@
 package com.pluralsight.model;
 
 public class Drink {
+    //Field
+   private String size;
+   private String flavor;
+   private double price;
+
+   // constructor
+    public Drink() {
+        this.size = size;
+        this.flavor = flavor;
+
+        if (size.equalsIgnoreCase("small")) {
+            price = 2.00;
+        } else if (size.equalsIgnoreCase("medium")) {
+            price = 2.50;
+        } else if (size.equalsIgnoreCase("large")) {
+            price = 3.00;
+        }
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "\n==== Drink ====" +
+                "\nSize: " + size +
+                "\nFlavor: " + flavor +
+                "\nPrice: $" + price;
+    }
 }
