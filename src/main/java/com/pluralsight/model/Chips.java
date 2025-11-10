0a0f44bc7c62d5e784db65fa888cbb06ebb18f42
mariannaaspuria <aspuria.mp@gmail.com>
2025-11-10 14:12:16 -0600

    update chips class commit

--- src/main/java/com/pluralsight/model/Chips.java
+++ src/main/java/com/pluralsight/model/Chips.java
@@ -1,4 +1,24 @@
 package com.pluralsight.model;
 
 public class Chips {
+    //Field
+    private String typeOfChip;
+    private double price = 1.50;
+
+    //constructor
+    public Chips(String typeOfChip) {
+        this.typeOfChip = typeOfChip;
+    }
+
+    //Getter
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "\n====Chips===="+
+                "\nChip type: " + typeOfChip +
+                "\nPrice: " + price;
+    }
 }
