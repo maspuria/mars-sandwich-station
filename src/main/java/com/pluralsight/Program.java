3cedccdaa8c639fd14e275c69a9fc6576425b928
mariannaaspuria <aspuria.mp@gmail.com>
2025-11-10 12:47:56 -0600

    update program commit

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,7 +1,11 @@
 package com.pluralsight;
 
+import com.pluralsight.ui.UserInterface;
+
 public class Program {
     public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+        ui.showHomeScreen();
 
     }
 }
