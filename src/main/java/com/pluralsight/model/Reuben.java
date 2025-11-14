9d348f1cde40cd44cfdc77725a47cd54f1eca600
mariannaaspuria <aspuria.mp@gmail.com>
2025-11-13 20:44:15 -0600

    signature sandwich reuben class extends sandwich class commit

---
+++ src/main/java/com/pluralsight/model/Reuben.java
@@ -0,0 +1,22 @@
+package com.pluralsight.model;
+
+public class Reuben extends Sandwich {
+    //constructor to define what Reuben sandwich is made of
+    public Reuben() {
+        setBreadType("Rye");
+        setSandwichSize(8);
+        setToasted(true);
+        setBasePriceSandwichSize();
+
+        addMeat("Roast Beef", false); // substituting for corn beef
+        addCheese("Swiss", false);
+        addTopping("Onions");
+        addSauce("Thousand Island");
+    }
+
+    @Override
+    public String getSummary() {
+        // returns custom title and getSummary() from the parent class to display sandwich summary
+       return "\n=============== Reuben Signature Sandwich ================== " + super.getSummary();
+    }
+}
